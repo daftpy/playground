2f41aa54e0f387b503deacb21cf123ecb55b2e3d
daftpy <daftpy@gmail.com>
2023-04-11 21:34:03 -0500

    Added prettier. Added a missing config for pm2 which runs the express server.

---
+++ dev-server/ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'daftpy',
+      script: 'app.js',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
+ 
